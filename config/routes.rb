9f8b756f6d1849cb0ebfd9a24de8509b2582074e
Stephen Bailey <sbailey@spotlink.com>
2015-07-06 16:04:34 -0700

    Finish login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :users
-  get 'users/new'
-
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
 end
