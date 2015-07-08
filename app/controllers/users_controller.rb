0d488024991a524f182f71b97f9b68f3fcacd874
Stephen Bailey <sbailey@spotlink.com>
2015-07-07 18:02:09 -0700

    Working on adding micropost functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   end
 
   def index
-  	@users = User.where(activated: true).order(:name).paginate(page: params[:page])
+  	@users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 	validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+	default_scope -> { order(:name) }
 
 	has_secure_password
+	has_many :microposts, dependent: :destroy
 
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
